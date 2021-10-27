37f717183c7eea3c3e234d6653466c0c938ba5c8
sam-timmins <84273109+sam-timmins@users.noreply.github.com>
2021-10-27 13:04:06 +0100

    Add map to interests

---
+++ assets/js/maps.js
@@ -0,0 +1,27 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: {
+            lat: 53.989515,
+            lng: -7.363234,
+        },
+        mapTypeId: 'satellite'
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 53.973862, lng: -7.354906 },
+        { lat: 53.972474, lng: -7.355442 },
+        { lat: 53.962148, lng: -7.358337 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
